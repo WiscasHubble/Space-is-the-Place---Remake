ae2356df6c4de7ae8693a609177923307d292640
WiscasHubble <luis.acevedosl@uanl.edu.mx>
2025-05-06 03:51:45 -0600

    Tab finalizada (v1.0 sujeto a cambios)

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FieldsetModule } from 'primeng/fieldset';
 import { ButtonModule } from 'primeng/button';
 import { HomecardsComponent } from "../../Components/homecards/homecards.component";
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-welcome',
-  imports: [FieldsetModule, ButtonModule, HomecardsComponent],
+  imports: [FieldsetModule, ButtonModule, HomecardsComponent, PanelModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
