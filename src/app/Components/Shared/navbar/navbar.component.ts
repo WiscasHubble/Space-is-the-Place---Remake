1e93518b62cf974e4dfe6362e6bbd1315764ee9a
WiscasHubble <luis.acevedosl@uanl.edu.mx>
2025-05-07 17:39:20 -0600

    Navbar terminada

--- src/app/Components/Shared/navbar/navbar.component.ts
+++ src/app/Components/Shared/navbar/navbar.component.ts
@@ -9,15 +9,19 @@ import { TabsModule } from 'primeng/tabs';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  imports: [TabsModule, RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule],
 })
 export class navbarComponent {
 
   tabs = [
-    { route: '', icon: 'pi pi-home', label: 'Home' },
+    { route: '/', icon: 'pi pi-home', label: 'Home' },
     { route: '/visits', icon: 'pi pi-map-marker', label: 'Visits' },
     { route: '/starships', icon: 'pi pi-rocket', label: 'Starships' },
     { route: '/about', icon: 'pi pi-info-circle', label: 'About' }
   ];
    
+  isActive(route: string): boolean {
+    return window.location.pathname === route || 
+           (route === '/' && window.location.pathname === '');
+  }
 }
--- src/app/Components/homecards/homecards.component.ts
+++ src/app/Components/homecards/homecards.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router'; // Añade esta importación
 
 @Component({
   selector: 'app-homecards',
-  imports: [CardModule, ButtonModule],
+  imports: [CardModule, ButtonModule, RouterModule],
   templateUrl: './homecards.component.html',
   styleUrl: './homecards.component.css'
 })
 export class HomecardsComponent {
-
+    tabs = [
+    { route: '/', icon: 'pi pi-home', label: 'Home' },
+    { route: '/visits', icon: 'pi pi-map-marker', label: 'Visits' },
+    { route: '/starships', icon: 'pi pi-rocket', label: 'Starships' },
+    { route: '/about', icon: 'pi pi-info-circle', label: 'About' }
+  ];
 }
